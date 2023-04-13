62b34aae4bc08300d0b6e25e03725ea7b3afd9aa
Zhur06 <zhur06@gmail.com>
2023-04-13 09:15:04 +0300

    + Student work tree

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -1,7 +1,7 @@
-<!-- Here must be NavTree but it needs users -->
+Here must be NavTree but it needs users
 <template>
   <aside id="menu">
-    <!-- <NavTree :work-tree="workTree" /> -->
+    <NavTree :work-tree="workTree" />
   </aside>
   <aside id="content">
     <h2>Это базовая страница</h2>
@@ -10,11 +10,42 @@
 </template>
 
 <script>
-// import NavTree from "../components/NavTree.vue";
+import axios from "axios";
+import NavTree from "../components/NavTree.vue";
 
-// export default {
-//   components: {
-//     NavTree,
-//   },
-// };
+export default {
+  data() {
+    return {
+      workTree: {}
+    }
+  },
+  components: {
+    NavTree,
+  },
+  methods: {
+    getUser() {
+      if (!this.$session.exists) {
+        this.$session.start();
+      }
+      const user_id = this.$session.get("user_id");
+      if (typeof user_id != "undefined") {
+        return user_id;
+      }
+      return -1;
+    },
+    async getWorkTree(user_id = this.user_id) {
+      if (user_id == -1) {
+        return "Not Logged in";
+      }
+      const { data: work_tree } = await axios.post("http://0.0.0.0:8179/get_student_worktree", {
+        id: user_id,
+      });
+      this.workTree = work_tree;
+      return work_tree;
+    },
+  },
+  created() {
+    this.getWorkTree(this.getUser());
+  },
+};
 </script>

4f3dab3544afaa5d1ddede1e8e8564cde67219e9
Zhur06 <zhur06@gmail.com>
2023-04-16 11:37:56 +0300

    ~ Some Teacher panel style changes

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -224,109 +224,111 @@ aside#foot {
   display: inline-block;
 }
 
-button#right-side, button#left-side {
-    height: 100%;
+button#right-side,
+button#left-side {
+  height: 100%;
 
-    background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8' standalone='no'%3F%3E%3C!-- Created with Inkscape (http://www.inkscape.org/) --%3E%3Csvg width='30' height='90' viewBox='0 0 7.9374999 23.8125' version='1.1' id='svg5' inkscape:version='1.2.1 (9c6d41e410, 2022-07-14)' sodipodi:docname='рисунок.svg' xmlns:inkscape='http://www.inkscape.org/namespaces/inkscape' xmlns:sodipodi='http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd' xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg'%3E%3Csodipodi:namedview id='namedview7' pagecolor='%23505050' bordercolor='%23eeeeee' borderopacity='1' inkscape:showpageshadow='0' inkscape:pageopacity='0' inkscape:pagecheckerboard='true' inkscape:deskcolor='%23505050' inkscape:document-units='mm' showgrid='false' inkscape:zoom='4.4195761' inkscape:cx='24.097334' inkscape:cy='68.106079' inkscape:window-width='1920' inkscape:window-height='1057' inkscape:window-x='-8' inkscape:window-y='-8' inkscape:window-maximized='1' inkscape:current-layer='layer1' /%3E%3Cdefs id='defs2'%3E%3Cinkscape:path-effect effect='fillet_chamfer' id='path-effect844' is_visible='true' lpeversion='1' nodesatellites_param='F,0,0,1,0,1.5875,0,1 @ F,0,0,1,0,1.5875,0,1 @ F,0,0,1,0,1.5875,0,1' unit='px' method='auto' mode='F' radius='6' chamfer_steps='1' flexible='false' use_knot_distance='true' apply_no_radius='true' apply_with_radius='true' only_selected='false' hide_knots='false' /%3E%3C/defs%3E%3Cg inkscape:label='Слой 1' inkscape:groupmode='layer' id='layer1'%3E%3Cpath sodipodi:type='star' style='fill:%23dadada;stroke:none;stroke-width:0.266999' id='path234' inkscape:flatsided='true' sodipodi:sides='3' sodipodi:cx='1.0177258' sodipodi:cy='11.90625' sodipodi:r1='13.26136' sodipodi:r2='6.6306801' sodipodi:arg1='0' sodipodi:arg2='1.0471976' inkscape:rounded='0' inkscape:randomized='0' d='m 12.904271,12.7 -17.1424099,9.897175 a 0.91654355,0.91654355 30 0 1 -1.3748154,-0.79375 l 0,-19.7943498 a 0.91654355,0.91654355 150 0 1 1.3748154,-0.79375 L 12.904271,11.1125 a 0.91654355,0.91654355 90 0 1 0,1.5875 z' inkscape:transform-center-x='-1.3229166' transform='matrix(0.39902895,0,0,1.0367076,2.2397312,-0.43705023)' inkscape:path-effect='%23path-effect844' /%3E%3C/g%3E%3C/svg%3E%0A");
-    background-repeat: no-repeat;
-    background-position: 50%;
+  background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8' standalone='no'%3F%3E%3C!-- Created with Inkscape (http://www.inkscape.org/) --%3E%3Csvg width='30' height='90' viewBox='0 0 7.9374999 23.8125' version='1.1' id='svg5' inkscape:version='1.2.1 (9c6d41e410, 2022-07-14)' sodipodi:docname='рисунок.svg' xmlns:inkscape='http://www.inkscape.org/namespaces/inkscape' xmlns:sodipodi='http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd' xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg'%3E%3Csodipodi:namedview id='namedview7' pagecolor='%23505050' bordercolor='%23eeeeee' borderopacity='1' inkscape:showpageshadow='0' inkscape:pageopacity='0' inkscape:pagecheckerboard='true' inkscape:deskcolor='%23505050' inkscape:document-units='mm' showgrid='false' inkscape:zoom='4.4195761' inkscape:cx='24.097334' inkscape:cy='68.106079' inkscape:window-width='1920' inkscape:window-height='1057' inkscape:window-x='-8' inkscape:window-y='-8' inkscape:window-maximized='1' inkscape:current-layer='layer1' /%3E%3Cdefs id='defs2'%3E%3Cinkscape:path-effect effect='fillet_chamfer' id='path-effect844' is_visible='true' lpeversion='1' nodesatellites_param='F,0,0,1,0,1.5875,0,1 @ F,0,0,1,0,1.5875,0,1 @ F,0,0,1,0,1.5875,0,1' unit='px' method='auto' mode='F' radius='6' chamfer_steps='1' flexible='false' use_knot_distance='true' apply_no_radius='true' apply_with_radius='true' only_selected='false' hide_knots='false' /%3E%3C/defs%3E%3Cg inkscape:label='Слой 1' inkscape:groupmode='layer' id='layer1'%3E%3Cpath sodipodi:type='star' style='fill:%23dadada;stroke:none;stroke-width:0.266999' id='path234' inkscape:flatsided='true' sodipodi:sides='3' sodipodi:cx='1.0177258' sodipodi:cy='11.90625' sodipodi:r1='13.26136' sodipodi:r2='6.6306801' sodipodi:arg1='0' sodipodi:arg2='1.0471976' inkscape:rounded='0' inkscape:randomized='0' d='m 12.904271,12.7 -17.1424099,9.897175 a 0.91654355,0.91654355 30 0 1 -1.3748154,-0.79375 l 0,-19.7943498 a 0.91654355,0.91654355 150 0 1 1.3748154,-0.79375 L 12.904271,11.1125 a 0.91654355,0.91654355 90 0 1 0,1.5875 z' inkscape:transform-center-x='-1.3229166' transform='matrix(0.39902895,0,0,1.0367076,2.2397312,-0.43705023)' inkscape:path-effect='%23path-effect844' /%3E%3C/g%3E%3C/svg%3E%0A");
+  background-repeat: no-repeat;
+  background-position: 50%;
 }
 
 button#left-side {
-    transform: rotate(180deg);
+  transform: rotate(180deg);
 }
 
 table.progress_line {
-    width: 100%;
-    border: 0px;
-    margin-bottom: 3px;
+  width: 100%;
+  border: 0px;
+  margin-bottom: 3px;
 }
 
 td.no-answer {
-    background-color: var(--no-answer-background-color);
+  background-color: var(--no-answer-background-color);
 }
 
 td.correct {
-    background-color: var(--correct-color);
+  background-color: var(--correct-color);
 }
 
 td.partial {
-    background-color: var(--unchecked-color);
+  background-color: var(--unchecked-color);
 }
 
 td.wrong {
-    background-color: var(--wrong-color);
+  background-color: var(--wrong-color);
 }
 
 #truth-table {
-    display: inline-grid;
-    border-radius: 10px;
-    border-width: 1px;
-    border-color: var(--dark-font-color);
-    overflow: hidden;
-    grid-template-columns: repeat(5, max-content);
-    grid-template-rows: 2em repeat(4, auto);
-    justify-items: center;
-    align-items: center;
+  display: inline-grid;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: var(--dark-font-color);
+  overflow: hidden;
+  grid-template-columns: repeat(5, max-content);
+  grid-template-rows: 2em repeat(4, auto);
+  justify-items: center;
+  align-items: center;
 }
 
 #truth-table * {
-    display: flex;
-    justify-content: center;
-    align-content: center;
+  display: flex;
+  justify-content: center;
+  align-content: center;
 }
 
 #truth-table div:not(:empty) {
-    padding: 5px;
-    width: 1.5em;
-    height: 1.2em;
+  padding: 5px;
+  width: 1.5em;
+  height: 1.2em;
 }
 
-#truth-table div:nth-of-type(10n+1), #truth-table div:nth-of-type(10n+2),
-#truth-table div:nth-of-type(10n+3), #truth-table div:nth-of-type(10n+4),
-#truth-table div:nth-of-type(10n+5) {
-    background-color: var(--content-hover-color);
+#truth-table div:nth-of-type(10n + 1),
+#truth-table div:nth-of-type(10n + 2),
+#truth-table div:nth-of-type(10n + 3),
+#truth-table div:nth-of-type(10n + 4),
+#truth-table div:nth-of-type(10n + 5) {
+  background-color: var(--content-hover-color);
 }
 
 connection {
-    height: 0;
-    display: block;
-    position: fixed;
-    top: 0;
-    left: 0;
+  height: 0;
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 
 connection .line .arrow {
-    top: -5px;
-    height: 0px;
-    width: 0px;
-    position: absolute;
-    border-bottom: 6px solid transparent;
-    border-top: 6px solid transparent;
-    background-clip: border-box;
+  top: -5px;
+  height: 0px;
+  width: 0px;
+  position: absolute;
+  border-bottom: 6px solid transparent;
+  border-top: 6px solid transparent;
+  background-clip: border-box;
 }
 connection .line .arrow-fw {
-    border-right: 12px solid black;
+  border-right: 12px solid black;
 }
 
 connection .line .arrow-bw {
-    left: 100%;
-    border-left: 12px solid black;
-    transform: translateX(-12px);
+  left: 100%;
+  border-left: 12px solid black;
+  transform: translateX(-12px);
 }
 
 connection .line .text {
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    transform: translate(-50%, -100%);
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translate(-50%, -100%);
 }
 
 connection .line {
-    position:absolute;
-    height:2px;
+  position: absolute;
+  height: 2px;
 
-    background-color: black;
+  background-color: black;
 }
-
 </style>
--- src/views/TeacherView.vue
+++ src/views/TeacherView.vue
@@ -1,9 +1,19 @@
 <template>
   <aside id="menu">
-    <RouterLink to="/teacher/users/">Все пользователи</RouterLink>
-    <RouterLink to="/teacher/groups/">Группы</RouterLink>
-    <RouterLink to="/teacher/works/">Работы</RouterLink>
-    <RouterLink to="/teacher/tasks/">Шаблоны задач</RouterLink>
+    <ul>
+      <li>
+        <RouterLink to="/teacher/users/">Все пользователи</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/teacher/groups/">Группы</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/teacher/works/">Работы</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/teacher/tasks/">Шаблоны задач</RouterLink>
+      </li>
+    </ul>
   </aside>
   <aside id="content">
     <RouterView />
@@ -14,276 +24,303 @@
 aside#menu,
 aside#content {
   position: absolute;
-  top: 65px;
-  bottom: 10px;
 
   overflow: scroll;
 }
 
 aside#menu {
-  left: 10px;
+  display: flex;
+  top: 60px;
+  bottom: 0px;
+  left: 0px;
 
-  width: calc(20% - 20px);
+  width: calc(20% - 10px);
 
-  padding: 10px;
-  border-radius: 20px;
+  justify-content: center;
+  padding: 0px;
+  border-radius: 0px 20px 20px 0px;
 
   background: var(--content-background-color);
   box-shadow: 4px 4px 4px var(--content-shadow-color);
 
   overflow-x: scroll;
 }
+
 #menu a {
   display: block;
 
-  margin: 5px 0px 0px 0px;
-
-  padding: 2px;
-  padding-bottom: 0px;
-  margin: 1px;
-
-  font-size: 0.9em;
   text-decoration: none;
   color: var(--body-font-color);
 }
 
-#menu a:hover {
-  background-color: var(--content-hover-color);
-}
-
-#menu .title {
-  font-size: large;
+#menu ul {
+  padding: 0px;
 }
 
 aside#content {
-  left: calc(20% + 20px);
-
-  width: calc(80% - 30px);
+  top: 65px;
+  bottom: 10px;
+  left: 20%;
+  right: 10px;
 
   border-radius: 20px;
   box-shadow: 4px 4px 4px var(--content-shadow-color);
   background: var(--content-background-color);
 }
 
-div#user-table, div#all-groups-table,
-div#task-table, div#task-field-table {
-    display: grid; 
-    grid-template-columns: auto auto 40px 40px;
-    
-    border-radius: 0 0 10px 10px;
-    overflow: hidden;
-    border-width: 1px;
-    border-color: var(--dark-font-color);
-    border-style: solid;
-    width: 100%;
+div#user-table,
+div#all-groups-table,
+div#task-table,
+div#task-field-table {
+  display: grid;
+  grid-template-columns: auto auto 40px 40px;
+
+  border-radius: 0 0 10px 10px;
+  overflow: hidden;
+  border-width: 1px;
+  border-color: var(--dark-font-color);
+  border-style: solid;
+  width: 100%;
 }
 
 div#user-table {
-    grid-template-columns: auto auto max-content 40px;
+  grid-template-columns: auto auto max-content 40px;
 }
 
 div#all-groups-table {
-    grid-template-columns: 100%;
+  grid-template-columns: 100%;
 }
 
 div#task-table {
-    grid-template-columns: auto 40px;
+  grid-template-columns: auto 40px;
 }
 
-.group-table-element, .user-table-element,
-.all-groups-table-element, .work-table-element,
-.task-table-element, #work-stats-table *,
-#task-field-table *, #groups-in-work-table * {
-    display: flex;
-    padding: 5px;
-    border-collapse: collapse;
+.group-table-element,
+.user-table-element,
+.all-groups-table-element,
+.work-table-element,
+.task-table-element,
+#work-stats-table *,
+#task-field-table *,
+#groups-in-work-table * {
+  display: flex;
+  padding: 5px;
+  border-collapse: collapse;
 }
 
-.group-table-element:nth-of-type(8n+3), .group-table-element:nth-of-type(8n+1),
-.group-table-element:nth-of-type(8n+2),
-.user-table-element:nth-of-type(8n+4), .user-table-element:nth-of-type(8n+1),
-.user-table-element:nth-of-type(8n+2), .user-table-element:nth-of-type(8n+3),
-.all-groups-table-element:nth-of-type(2n+1),
-.work-table-element:nth-of-type(6n+1), .work-table-element:nth-of-type(6n+2),
-.work-table-element:nth-of-type(6n+3),
-#groups-in-work-table *:nth-of-type(2n+1),
-#work-stats-table *:nth-of-type(10n+1), #work-stats-table *:nth-of-type(10n+2),
-#work-stats-table *:nth-of-type(10n+3), #work-stats-table *:nth-of-type(10n+4),
-#work-stats-table *:nth-of-type(10n+5),
-#task-field-table *:nth-of-type(4n+1), #task-field-table *:nth-of-type(4n+2),
-.task-table-element:nth-of-type(4n+1), .task-table-element:nth-of-type(4n+2) {
-    background-color: var(--content-hover-color);
+.group-table-element:nth-of-type(8n + 3),
+.group-table-element:nth-of-type(8n + 1),
+.group-table-element:nth-of-type(8n + 2),
+.user-table-element:nth-of-type(8n + 4),
+.user-table-element:nth-of-type(8n + 1),
+.user-table-element:nth-of-type(8n + 2),
+.user-table-element:nth-of-type(8n + 3),
+.all-groups-table-element:nth-of-type(2n + 1),
+.work-table-element:nth-of-type(6n + 1),
+.work-table-element:nth-of-type(6n + 2),
+.work-table-element:nth-of-type(6n + 3),
+#groups-in-work-table *:nth-of-type(2n + 1),
+#work-stats-table *:nth-of-type(10n + 1),
+#work-stats-table *:nth-of-type(10n + 2),
+#work-stats-table *:nth-of-type(10n + 3),
+#work-stats-table *:nth-of-type(10n + 4),
+#work-stats-table *:nth-of-type(10n + 5),
+#task-field-table *:nth-of-type(4n + 1),
+#task-field-table *:nth-of-type(4n + 2),
+.task-table-element:nth-of-type(4n + 1),
+.task-table-element:nth-of-type(4n + 2) {
+  background-color: var(--content-hover-color);
 }
 
-.user-table-element a, .all-groups-table-element a,
-.task-table-element a, #task-field-table a,
+.user-table-element a,
+.all-groups-table-element a,
+.task-table-element a,
+#task-field-table a,
 #work-table a {
-    text-decoration: none;
-    color: var(--body-font-color);
+  text-decoration: none;
+  color: var(--body-font-color);
 }
 
-div#user-table-header, div#group-table-header,
-div#all-groups-table-header, div#work-table-header,
-div#task-table-header, div#work-stats-table-header,
-div#task-field-table-header, div#groups-in-work-table-header {
-    display: grid;
-    border-radius: 10px 10px 0 0;
-    overflow: hidden;
-    border-width: 1px;
-    border-color: var(--dark-font-color);
-    border-style: solid;
-    width: calc(100% - 22px);
-    margin-left: 10px;
-    grid-template-columns: auto max-content max-content;
-    border-bottom: 0;
+div#user-table-header,
+div#group-table-header,
+div#all-groups-table-header,
+div#work-table-header,
+div#task-table-header,
+div#work-stats-table-header,
+div#task-field-table-header,
+div#groups-in-work-table-header {
+  display: grid;
+  border-radius: 10px 10px 0 0;
+  overflow: hidden;
+  border-width: 1px;
+  border-color: var(--dark-font-color);
+  border-style: solid;
+  width: calc(100% - 22px);
+  margin-left: 10px;
+  grid-template-columns: auto max-content max-content;
+  border-bottom: 0;
 }
 
 div#work-stats-table-header {
-    grid-template-columns: repeat(2, auto) min-content;
+  grid-template-columns: repeat(2, auto) min-content;
 }
 
 div#work-stats-table-header.detached {
-    border-radius: 10px;
-    border-bottom: 1px solid var(--dark-font-color);
-    margin-bottom: 10px;
+  border-radius: 10px;
+  border-bottom: 1px solid var(--dark-font-color);
+  margin-bottom: 10px;
 }
 
-div#user-table-header, div#user-table,
-div#all-groups-table, div#all-groups-table-header,
-div#task-table, div#task-table-header,
-div#task-field-table, div#task-field-table-header {
-    border: none;
-    width: 100%;
-    margin-left: 0;
+div#user-table-header,
+div#user-table,
+div#all-groups-table,
+div#all-groups-table-header,
+div#task-table,
+div#task-table-header,
+div#task-field-table,
+div#task-field-table-header {
+  border: none;
+  width: 100%;
+  margin-left: 0;
 }
 
-#user-table-header *, #all-groups-table-header *,
+#user-table-header *,
+#all-groups-table-header *,
 #task-table-header * {
-    display: inline-flex;
-    align-items: center;
-    padding: 4px;
+  display: inline-flex;
+  align-items: center;
+  padding: 4px;
 }
 
-div#group-table, div#work-table,
-div#work-stats-table, div#groups-in-work-table {
-    display: grid; 
-    grid-template-columns: auto auto 40px 40px;
-    
-    border-radius: 0 0 10px 10px;
-    overflow: hidden;
-    border-width: 1px;
-    border-color: var(--dark-font-color);
-    border-style: solid;
-    width: calc(100% - 22px);
+div#group-table,
+div#work-table,
+div#work-stats-table,
+div#groups-in-work-table {
+  display: grid;
+  grid-template-columns: auto auto 40px 40px;
 
-    margin-bottom: 10px;
-    margin-left: 10px;
+  border-radius: 0 0 10px 10px;
+  overflow: hidden;
+  border-width: 1px;
+  border-color: var(--dark-font-color);
+  border-style: solid;
+  width: calc(100% - 22px);
+
+  margin-bottom: 10px;
+  margin-left: 10px;
 }
 
-div#work-table, div#group-table {
-    grid-template-columns: auto auto 40px;
+div#work-table,
+div#group-table {
+  grid-template-columns: auto auto 40px;
 }
 
 div#work-stats-table {
-    grid-template-columns: repeat(5, auto);
+  grid-template-columns: repeat(5, auto);
 }
 
 div#task-field-table {
-    grid-template-columns: repeat(2, auto);
+  grid-template-columns: repeat(2, auto);
 }
 
 div#groups-in-work-table {
-    grid-template-columns: auto;
+  grid-template-columns: auto;
 }
 
 div#task-manage-table {
-    display: grid;
-    grid-template-columns: calc(50% - 5px) 10px calc(50% - 5px);
-    align-items: baseline;
-    overflow: hidden;
+  display: grid;
+  grid-template-columns: calc(50% - 5px) 10px calc(50% - 5px);
+  align-items: baseline;
+  overflow: hidden;
 
-    border-radius: 10px;
-    border-width: 1px;
-    border-color: var(--dark-font-color);
-    border-style: solid;
-    width: calc(100% - 20px);
-    
-    top: 115px;
-    bottom: 10px;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: var(--dark-font-color);
+  border-style: solid;
+  width: calc(100% - 20px);
+
+  top: 115px;
+  bottom: 10px;
 }
 
 #task-manage-table *::-webkit-scrollbar-track {
-    margin: 0;
+  margin: 0;
 }
 
 #task-manage-table-column-header p {
-    padding: 0px;
+  padding: 0px;
 }
 
 #task-manage-table div.highlighted {
-    border-radius: 10px;
-    padding: 5px;
-    height: calc(100% - 10px);
+  border-radius: 10px;
+  padding: 5px;
+  height: calc(100% - 10px);
 
-    background-color: var(--content-hover-color);
+  background-color: var(--content-hover-color);
 }
 
 #task-manage-table div:nth-of-type(2n + 1) div {
-    overflow: hidden;
-    padding: 0px;
-    border-radius: 0px;
-    height: fit-content;
+  overflow: hidden;
+  padding: 0px;
+  border-radius: 0px;
+  height: fit-content;
 }
 
 div#task-manage-table-column-header {
-    position: sticky;
-    display: grid;
-    grid-template-columns: auto max-content min-content;
+  position: sticky;
+  display: grid;
+  grid-template-columns: auto max-content min-content;
 
-    overflow: hidden;
+  overflow: hidden;
 
-    width: 100%;
-    height: 36px;
-    padding: 0px;
-    top: 0px;
-    left: 0px;
+  width: 100%;
+  height: 36px;
+  padding: 0px;
+  top: 0px;
+  left: 0px;
 
-    font-size: large;
+  font-size: large;
 }
 
 div#task-manage-table-column-header * {
-    margin: 0 0 0 10px;
+  margin: 0 0 0 10px;
 }
 
 div#task-manage-table-column-header * * {
-    margin: 0;
+  margin: 0;
 }
 
-#group-table-header *, #work-table-header *,
-#work-stats-table-header *, #task-field-table-header *,
+#group-table-header *,
+#work-table-header *,
+#work-stats-table-header *,
+#task-field-table-header *,
 #groups-in-work-table-header * {
-    display: flex;
-    align-items: center;
-    padding: 4px;
-    margin: 1px;
+  display: flex;
+  align-items: center;
+  padding: 4px;
+  margin: 1px;
 }
 
-#group-manage-header, #task-manage-header,
+#group-manage-header,
+#task-manage-header,
 #work-manage-header {
-    display: grid;
-    grid-template-columns: auto max-content;
-    align-items: center;
+  display: grid;
+  grid-template-columns: auto max-content;
+  align-items: center;
 }
 
-.group-info:disabled, .user-table-element input:disabled, input:disabled {
-    border: none;
-    color: currentColor;
-    background-color: transparent !important;
+.group-info:disabled,
+.user-table-element input:disabled,
+input:disabled {
+  border: none;
+  color: currentColor;
+  background-color: transparent !important;
 }
 
-#group-manage-header div:nth-of-type(2n), #task-manage-header div:nth-of-type(2n),
+#group-manage-header div:nth-of-type(2n),
+#task-manage-header div:nth-of-type(2n),
 #work-manage-header div:nth-of-type(2n) {
-    justify-items: end;
-    justify-self: end;
+  justify-items: end;
+  justify-self: end;
 }
 </style>

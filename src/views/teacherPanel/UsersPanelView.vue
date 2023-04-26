fb968de22d2d9dbbd56d077bd14f59ee0b01e849
Zhur06 <zhur06@gmail.com>
2023-04-26 13:02:46 +0300

    ~ Speed of animations

--- src/views/teacherPanel/UsersPanelView.vue
+++ src/views/teacherPanel/UsersPanelView.vue
@@ -244,7 +244,7 @@ tr:hover button.button-icon {
   justify-items: center;
   display: inline-grid;
   grid-template-columns: 1.5em auto;
-  transition: width 1s ease-in-out 0.5s;
+  transition: width 80ms ease-in-out 360ms;
 }
 
 tr .button-icon {
@@ -255,7 +255,7 @@ tr .button-icon {
 .button-icon > span {
   opacity: 0;
   transform: translateX(12em);
-  transition: all 0.75s ease-in-out 0.75s;
+  transition: all 40ms ease-in-out 380ms;
 }
 
 .button-icon:hover {
